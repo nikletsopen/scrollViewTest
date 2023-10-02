ed58c7ed88e5f5cc25b118c315b7fb4d1327a0e5
Nikita Timonin <nikita@letsopen.co>
2023-10-02 14:40:03 +0300

    Initial commit

--- ScrollViewTest/ContentView.swift
+++ ScrollViewTest/ContentView.swift
@@ -9,18 +9,39 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        TabView {
+            ScrollView()
+                .tabItem {
+                    Label("First", systemImage: "star")
+                }
         }
-        .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct ScrollView: View {
+    var body: some View {
+        List {
+            ForEach((1...100), id: \.self) {
+                Text("Row \($0)")
+            }
+        }
+        .safeAreaInset(edge: .bottom, spacing: 0) {
+            // some bottom-sticked content, or just –
+            Spacer()
+                .frame(height: 50)
+        }
+        .overlay(alignment: .bottom) {
+            Color(.red)
+                .frame(height: 50.0)
+                .opacity(0.4)
+        }
+        .listStyle(.plain)
+       
     }
 }
