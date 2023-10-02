1f5d3637eabb689f14af77abf7fa2313f6cbed2a
Nikita Timonin <nikita@letsopen.co>
2023-10-02 14:02:07 +0300

    Initial Commit

---
+++ ScrollViewTest/ScrollViewTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollViewTestApp.swift
+//  ScrollViewTest
+//
+//  Created by Nikita Timonin on 02.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct ScrollViewTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
